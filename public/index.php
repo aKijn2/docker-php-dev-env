9f6393b6533238a2095ddea545b93e25ea138960
aKijn2 <mrkinder20041010@gmail.com>
2024-02-07 10:18:43 +0100

    ready / adding astro soon

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Froga</title>
+</head>
+<body>
+    <div>
+        <h1>PHP Froga</h1>
+        <p>PHP Froga</p>
+    </div>
+</body>
+</html>
